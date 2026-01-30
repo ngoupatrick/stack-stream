34ff4868215d1efe95c9f3ab76f738db4e1bfc8f
Patrick <ngounepatrick@gmail.com>
2026-01-30 16:21:39 +0000

    first commit

---
+++ setup-connectors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "🚀 Chargement du connecteur Source Postgres..."
+curl -X POST http://localhost:8083/connectors \
+     -H "Content-Type: application/json" \
+     -d @source-postgres.json
+
+echo -e "\n🚀 Chargement du connecteur Sink Elasticsearch..."
+curl -X POST http://localhost:8083/connectors \
+     -H "Content-Type: application/json" \
+     -d @sink-elastic.json
+
+echo -e "\n✅ Terminé ! Vérifiez l'état avec : curl http://localhost:8083/connectors?expand=status"
+
